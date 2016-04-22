3d5344590f66cf331b81e0412304f481eb9c1cec
GaoZhen <912815282@qq.com>
2016-04-22 17:37:38 +0800

    去掉注释

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -15,24 +15,15 @@ import org.hibernate.Transaction;
 import model.Department;
 import model.Hospital;
 
-/**
- * Servlet implementation class RegisterServlet
- */
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public RegisterServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		SessionFactory sessionFactory = hibernate.Factory.get();
 		Session session = sessionFactory.openSession();
