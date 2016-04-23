cd44a0426149491c9eb0ecefb9e4ba92de56aaad
yjc <1551251072@qq.com>
2016-04-24 00:28:18 +0800

    gzsb

--- src/servlet/RegisterServlet.java
+++ src/servlet/RegisterServlet.java
@@ -21,18 +21,16 @@ import model.Hospital;
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public RegisterServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		SessionFactory sessionFactory = hibernate.Factory.get();
 		Session session = sessionFactory.openSession();
