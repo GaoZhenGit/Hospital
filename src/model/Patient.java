d63b7b9e62ed6439669db0507c98ae407cd6f26b
GaoZhen <912815282@qq.com>
2016-04-25 09:20:47 +0800

    bank account

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -4,6 +4,7 @@ public class Patient extends Person{
 	
 	private String accout;
 	private String password;
+	private String bankAccount;
 	
 	
 	public String getAccout() {
@@ -17,5 +18,11 @@ public class Patient extends Person{
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getBankAccount() {
+		return bankAccount;
+	}
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
 	}
 }
